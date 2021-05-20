ebc4a09647f2e8b8245c69ce2dc758fdd951a35b
isra9shadow <isra9movil@hotmail.com>
2021-05-20 16:10:57 +0200

    Verificacion email

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,9 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('verified');
 
-Auth::routes();
+Auth::routes(['verify' =>true ]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
